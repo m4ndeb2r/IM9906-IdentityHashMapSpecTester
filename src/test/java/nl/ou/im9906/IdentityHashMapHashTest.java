2bc18437191474df51691422fb7651debbd236f7
martindeboer <m4ndeb2r@gmail.com>
2021-01-30 13:37:05 +0100

    Extended test for hash method (after extending JML).

--- src/test/java/nl/ou/im9906/IdentityHashMapHashTest.java
+++ src/test/java/nl/ou/im9906/IdentityHashMapHashTest.java
@@ -1,11 +1,18 @@
 package nl.ou.im9906;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.IdentityHashMap;
 
 import static nl.ou.im9906.ClassInvariantTestHelper.assertClassInvariants;
 import static nl.ou.im9906.MethodTestHelper.assertIsPureMethod;
+import static nl.ou.im9906.ReflectionUtils.invokeStaticMethodWithParams;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.Is.is;
 
 /**
  * Tests the JML specifications of the {@link IdentityHashMap#hash(Object, int)}
@@ -19,6 +26,23 @@ public class IdentityHashMapHashTest {
      * as specified in the JML. Furthermore, as a precondition as well a postcondition,
      * the class invariants should hold, obviously.
      *
+     * The JML being tested:
+     * <pre>
+     *   private normal_behavior
+     *     requires
+     *       x != null;
+     *     ensures
+     *       \result == \dl_genHash(x, length) &&
+     *       \result < length &&
+     *       \result >=0;
+     *   also
+     *     private normal_behavior
+     *       requires
+     *         x == null;
+     *       ensures
+     *         \result == 0;
+     * </pre>
+     *
      * @throws NoSuchFieldException   if one or more fields do not exist
      * @throws IllegalAccessException if one or more field cannot be accessed
      * @throws NoSuchMethodException  if the method to invoke does not exist
@@ -27,12 +51,21 @@ public class IdentityHashMapHashTest {
     @Test
     public void testHashNormalBehaviour()
             throws NoSuchMethodException, IllegalAccessException,
-            NoSuchFieldException, NoSuchClassException {
+            NoSuchFieldException, NoSuchClassException, InvocationTargetException {
         // Create a new IdentityHashMap
         final IdentityHashMap<Object, Object> map = new IdentityHashMap<>();
 
         // Test if the class invariants hold (precondition)
         assertClassInvariants(map);
+
+        // Test the return value of the hash method when first param == null
+        final Integer zeroHash = (Integer) invokeStaticMethodWithParams(IdentityHashMap.class, "hash", null, 256);
+        assertThat(zeroHash, is(0));
+
+        // Test the return value of the hash method when first param != null
+        final Integer hash = (Integer) invokeStaticMethodWithParams(IdentityHashMap.class, "hash", new Object(), 2);
+        assertThat(hash, lessThan(2));
+        assertThat(hash, greaterThanOrEqualTo(0));
 
         // Call the hash method, and check if it is pure
         assertIsPureMethod(map, "hash", new Object(), 32);
