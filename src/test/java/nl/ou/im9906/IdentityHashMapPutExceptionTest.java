639c1a25b3a7b3f838cebba448f5aca8cba6ae73
martindeboer <m4ndeb2r@gmail.com>
2021-10-25 17:58:51 +0200

    Fine-tuned the test to see what happens after put throws an exception (via resize()). Proves that an infinite loop will occur in put();

--- src/test/java/nl/ou/im9906/IdentityHashMapPutExceptionTest.java
+++ src/test/java/nl/ou/im9906/IdentityHashMapPutExceptionTest.java
@@ -1,23 +1,14 @@
 package nl.ou.im9906;
 
 import junit.framework.AssertionFailedError;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.util.HashMap;
 import java.util.IdentityHashMap;
-import java.util.Map;
 
 import static nl.ou.im9906.ClassInvariantTestHelper.assertClassInvariants;
-import static nl.ou.im9906.MethodTestHelper.assertAssignableClause;
 import static nl.ou.im9906.ReflectionUtils.getValueByFieldName;
 import static nl.ou.im9906.ReflectionUtils.getValueByStaticFieldName;
-import static nl.ou.im9906.ReflectionUtils.setValueByFieldName;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
-import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.fail;
 
 /**
  * Tests the JML specifications of the {@link IdentityHashMap#put(Object, Object)}
@@ -30,30 +21,72 @@ public class IdentityHashMapPutExceptionTest {
     @Test
     public void testConstructorWithMapContainingMaxCapacityOfElements()
             throws NoSuchFieldException, IllegalAccessException, NoSuchClassException {
+
         final int maxCapacity = (int) getValueByStaticFieldName(SmallIdentityHashMapForPutException.class, "MAXIMUM_CAPACITY");
-        final Map<String, String> constructorInputMap = new HashMap<String, String>();
+        map = new SmallIdentityHashMapForPutException<String, String>();
+
+        // Add MAXIMUM_CAPACIY - 2 unique elements to the map
         for (int i = 0; i < maxCapacity - 2; i++) {
-            constructorInputMap.put("key_" + i, "value_" + i);
+            map.put(new String("key_" + (i + 1)), new String("value_" + (i + 1)));
         }
-        map = new SmallIdentityHashMapForPutException<String, String>(constructorInputMap);
 
+        // At this point, there are two empty entries. The class invariant still holds
+        assertClassInvariants(map);
+
+        // Now, keep adding entries, and ignore the IllegalStateExceptions thrown by resize().
+        // The class invariant will be violated, and the put method will subsequently loop infinitely.
         for (int j = 0; j < 5; j++) {
-            try {
-                System.out.println("Size voor put: " + map.size());
-                System.out.println("Put .....");
-                map.put(new String("BOOM! BOOM!"), "Out go the lights!");
-                System.out.println("Geen exception.");
-                System.out.println("=====================================");
-            } catch (IllegalStateException e) {
-                System.out.println("Boem!");
-                System.out.println("Size na mislukte put: " + map.size());
-                System.out.println("=====================================");
+            // Print the state beforehand
+            System.out.println("");
+            printState(map);
+            System.out.println("Executing put(key_" + (map.size() + 1) + ", value_" + (map.size() + 1) + ") ....");
 
-                // Test the class invariant if exception has occurred
-                assertClassInvariants(map);
+            // Try to put an entry into the map
+            try {
+                map.put("key_" + map.size() + 1, "value_" + map.size() + 1);
+                fail("No exception was thrown. This was not expected.");
+            } catch (IllegalStateException e) {
+                System.out.println("IllegalStateException occurred as expected (via resize).");
             }
+
+            // Print the state afterwards
+            printState(map);
+        }
+    }
+
+    private void printState(SmallIdentityHashMapForPutException<String, String> map)
+            throws NoSuchFieldException, IllegalAccessException, NoSuchClassException {
+        final int maxCapacity = (int) getValueByStaticFieldName(SmallIdentityHashMapForPutException.class, "MAXIMUM_CAPACITY");
+        final Object[] table = (Object[]) getValueByFieldName(map, "table");
+        final int threshold = (int) getValueByFieldName(map, "threshold");
+        boolean isClassInvariantViolated = false;
+        try {
+            assertClassInvariants(map);
+        } catch (AssertionError e) {
+            isClassInvariantViolated = true;
         }
 
+        System.out.println("Current state of " + map.getClass().getSimpleName());
+        System.out.println("- size = " + map.size());
+        System.out.println("- table.length = " + table.length);
+        System.out.println("- threshold = " + threshold);
+        System.out.println("- max capacity = " + maxCapacity);
+        System.out.println("- number of empty entries = " + emptyEntries(table));
+        if (isClassInvariantViolated) {
+            System.out.println("- class invariant is violated!!!");
+        } else {
+            System.out.println("- class invariant holds");
+        }
+
+    }
+
+    private int emptyEntries(Object[] table)
+            throws NoSuchFieldException, IllegalAccessException {
+        int count = 0;
+        for (int i = 0; i < table.length; i += 2) {
+            if (table[i] == null) count++;
+        }
+        return count;
     }
 
 }
