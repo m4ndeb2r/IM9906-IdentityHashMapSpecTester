5fe94b9b470f3b80f708ae722697c00ae7db2a91
martindeboer <m4ndeb2r@gmail.com>
2021-10-25 21:27:05 +0200

    Fine-tuned the test to see what happens after put throws an exception (via resize()). Added output that shows the violated invariant clauses.

--- src/test/java/nl/ou/im9906/IdentityHashMapPutExceptionTest.java
+++ src/test/java/nl/ou/im9906/IdentityHashMapPutExceptionTest.java
@@ -1,6 +1,5 @@
 package nl.ou.im9906;
 
-import junit.framework.AssertionFailedError;
 import org.junit.Test;
 
 import java.util.IdentityHashMap;
@@ -8,6 +7,7 @@ import java.util.IdentityHashMap;
 import static nl.ou.im9906.ClassInvariantTestHelper.assertClassInvariants;
 import static nl.ou.im9906.ReflectionUtils.getValueByFieldName;
 import static nl.ou.im9906.ReflectionUtils.getValueByStaticFieldName;
+import static nl.ou.im9906.ReflectionUtils.hash;
 import static org.junit.Assert.fail;
 
 /**
@@ -73,7 +73,8 @@ public class IdentityHashMapPutExceptionTest {
         System.out.println("- max capacity = " + maxCapacity);
         System.out.println("- number of empty entries = " + emptyEntries(table));
         if (isClassInvariantViolated) {
-            System.out.println("- class invariant is violated!!!");
+            System.out.println("- class invariant is violated!!! Violated clauses are:");
+            printViolatedInvariantClauses(map, table);
         } else {
             System.out.println("- class invariant holds");
         }
@@ -87,6 +88,151 @@ public class IdentityHashMapPutExceptionTest {
             if (table[i] == null) count++;
         }
         return count;
+    }
+
+    private void printViolatedInvariantClauses(SmallIdentityHashMapForPutException<String, String> map, Object[] table) throws NoSuchFieldException, IllegalAccessException {
+        final int maxCapacity = (int) getValueByStaticFieldName(SmallIdentityHashMapForPutException.class, "MAXIMUM_CAPACITY");
+        final int minCapacity = (int) getValueByStaticFieldName(SmallIdentityHashMapForPutException.class, "MINIMUM_CAPACITY");
+        final int threshold = (int) getValueByFieldName(map, "threshold");
+
+        // Class invariant for IdentityHashMap:
+        //    table != null &&
+        //    MINIMUM_CAPACITY * 2 <= table.length &&
+        //    MAXIMUM_CAPACITY * 2 >= table.length
+        if (table == null)
+            System.out.println("\t- table != null");
+        if (table.length < minCapacity * 2)
+            System.out.println("\t- MINIMUM_CAPACITY * 2 <= table.length");
+        if (table.length > maxCapacity * 2)
+            System.out.println("\t- MAXIMUM_CAPACITY * 2 >= table.length");
+
+        // Class invariant for IdentityHashMap:
+        //    (\forall int i;
+        //        0 <= i && i < table.length - 1;
+        //        i % 2 == 0 ==> (table[i] == null ==> table[i + 1] == null));
+        // If the key is null, than the value must also be null
+        for (int i = 0; i < table.length - 1; i += 2) {
+            if (table[i] == null && table[i + 1] != null)
+                System.out.println("\t- if the key is null, than the value must also be null");
+        }
+
+        // Class invariant for IdentityHashMap:
+        //    (\forall int i; 0 <= i && i < table.length / 2;
+        //       (\forall int j;
+        //         i <= j && j < table.length / 2;
+        //        (table[2*i] != null && table[2*i] == table[2*j]) ==> i == j));
+        // Every none-null key is unique
+        for (int i = 0; i < table.length / 2; i++) {
+            if (table[2 * i] == null) continue; // Performance+
+            for (int j = i; j < table.length / 2; j++) {
+                if (table[2 * i] != null && table[2 * i] == table[2 * j]) {
+                    if (i != j)
+                        System.out.println("\t- every none-null key must be unique");
+                }
+            }
+        }
+
+        // Class invariant for IdentityHashMap:
+        //     size == (\num_of int i;
+        //        0 <= i < table.length /2;
+        //        table[2*i] != null)
+        // Size equals number of none-null keys in table
+        int expectedSize = 0;
+        for (int i = 0; i < table.length / 2; i++) {
+            if (table[2 * i] != null) {
+                expectedSize++;
+            }
+        }
+        if (map.size() != expectedSize)
+            System.out.println("\t- size must equal the number of none-null keys in table");
+
+        // Class invariant for IdentityHashMap
+        //   (\exists int i;
+        //     0 <= i < table.length;
+        //        \dl_pow(2,i) == table.length);
+        // Table length is a power of two
+        if (!((table.length & -table.length) == table.length))
+            System.out.println("\t- table.length must be a power of two");
+
+        // Class invariant for IdentityHashMap
+        //   (\exists int i;
+        //     0 <= i < table.length / 2;
+        //     table[2*i] == null);
+        // Table must have at least one empty key-element to prevent
+        // infinite loops when a key is not present.
+        boolean hasEmptyKey = false;
+        for (int i = 0; i < table.length / 2; i++) {
+            if (table[2 * i] == null) {
+                hasEmptyKey = true;
+                break;
+            }
+        }
+        if (!hasEmptyKey)
+            System.out.println("\t- table must have at least one empty key-element to prevent infinite loops when a key is not present");
+
+        // Class invariant for IdentityHashMap
+        //   (\forall int i;
+        //     0 <= i < table.length / 2;
+        //       table[2*i] != null && 2*i > hash(table[2*i], table.length) ==>
+        //       (\forall int j;
+        //         hash(table[2*i], table.length) <= 2*j < 2*i;
+        //         table[2*j] != null));
+        // There are no gaps between a key's hashed index and its actual
+        // index (if the key is at a higher index than the hash code)
+        for (int i = 0; i < table.length / 2; i++) {
+            final int hash = hash(table[2 * i], table.length);
+            if (table[2 * i] != null && 2 * i > hash) {
+                for (int j = hash / 2; j < i; j++) {
+                    if (!(table[2 * j] != null))
+                        System.out.println("\t- there are no gaps between a key's hashed index and its actual index (if the key is at a higher index than the hash code)");
+                }
+            }
+        }
+
+        // Class invariant for IdentityHashMap
+        //   (\forall int i;
+        //     0 <= i < table.length / 2;
+        //     table[2*i] != null && 2*i < hash(table[2*i], table.length) ==>
+        //     (\forall int j;
+        //       hash(table[2*i], table.length) <= 2*j < table.length || 0 <= 2*j < 2*i;
+        //       table[2*j] != null));
+        // There are no gaps between a key's hashed index and its actual
+        // index (if the key is at a lower index than the hash code)
+        for (int i = 0; i < table.length / 2; i++) {
+            final int hash = hash(table[2 * i], table.length);
+            if (table[2 * i] != null && 2 * i < hash) {
+                for (int j = hash / 2; j < table.length / 2; j++) {
+                    if (table[2 * j] == null)
+                        System.out.println("\t- there are no gaps between a key's hashed index and its actual index (if the key is at a lower index than the hash code)");
+                }
+                for (int j = 0; j < i; j++) {
+                    if (table[2 * j] == null)
+                        System.out.println("\t- there are no gaps between a key's hashed index and its actual index (if the key is at a lower index than the hash code)");
+                }
+            }
+        }
+
+        // Class invariant  for IdentityHashMap
+        //   size <= threshold &&
+        //   size == threshold ==> size == MAXIMUM_CAPACITY - 1;
+        // Bounds on size in relation to threshold
+        if (map.size() > threshold)
+            System.out.println("\t- size <= threshold");
+        if (map.size() == threshold) {
+            if (map.size() != (maxCapacity - 1))
+                System.out.println("\t- size == threshold ==> size == MAXIMUM_CAPACITY - 1");
+        }
+
+        // Class invariant  for IdentityHashMap
+        //   (threshold == table.length / 3 || threshold == MAXIMUM_CAPACITY - 1) &&
+        //   table.length < 2 * MAXIMUM_CAPACITY ==> threshold == table.length / 3;
+        // Bounds on threshold in relation to table.length and MAXIMUM_CAPACITY
+        if (!(threshold == table.length / 3 || threshold == maxCapacity - 1))
+            System.out.println("\t- bounds on threshold in relation to table.length and MAXIMUM_CAPACITY");
+        if (table.length < maxCapacity * 2) {
+            if (!(threshold == (table.length / 3)))
+                System.out.println("\t- bounds on threshold in relation to table.length and MAXIMUM_CAPACITY");
+        }
     }
 
 }
